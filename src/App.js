959359d076eadde028d84197f578e400a0135ce5
Arslan  Ibrahim <abrahum9328@gmail.com>
2024-09-24 16:58:25 +0500

    Added

---
+++ src/App.js
@@ -0,0 +1,68 @@
+import React from 'react'
+import Logo1 from "./assets/w-whatsapp-1-logo-removebg-preview.png"
+import Logo2 from "./assets/w-whatsapp-2-logo-removebg-preview.png"
+import { FaArrowRight } from "react-icons/fa6"
+
+function App() {
+  return (
+    <div className="w-full min-h-screen pb-10">
+      {/* WhatsApp Group Section */}
+      <div className="w-full md:w-[90%] mx-auto mt-6 flex flex-wrap justify-center xl:justify-between lg:justify-between md:justify-between sm:justify-between  items-center flex-col xl:flex-row lg:flex-row md:flex-col  p-4 shadow-lg rounded-lg bg-white text-red-500">
+        <div className=" flex items-center flex-wrap flex-col xl:flex-row lg:flex-row">
+          <h1 className='text-xl md:text-2xl lg:text-3xl text1 font-bold'>Click this link to join WhatsApp Group</h1>
+          <div className='flex space-x-4 mt-5 xl:mt-0'>
+            <div className="icon h-10 w-10  text-2xl md:text-3xl flex justify-center items-center bg-red-500 text-white rounded-md ml-3 md:ml-5">
+              <FaArrowRight />
+            </div>
+            <div className="icon h-10 w-10  text-2xl md:text-3xl  flex justify-center items-center bg-red-500 text-white rounded-md ml-3 md:ml-5">
+              <FaArrowRight />
+            </div>
+
+          </div>
+        </div>
+        <a href="https://chat.whatsapp.com/ElzWGqAuASp57bOhevsC8y" className="h-20 w-20 md:h-24 md:w-28 block mt-6 xl:mt-0">
+          <img src={Logo1} className="w-full h-full" alt="WhatsApp" />
+        </a>
+      </div>
+
+      {/* Second WhatsApp Group Section */}
+      <div className="w-full md:w-[90%] mx-auto mt-6 flex flex-wrap justify-center xl:justify-between lg:justify-between md:justify-between sm:justify-between  items-center flex-col xl:flex-row lg:flex-row md:flex-col  p-4 shadow-lg rounded-lg bg-white text-red-500">
+        <div className=" flex items-center flex-wrap flex-col xl:flex-row lg:flex-row">
+          <h1 className='text-xl md:text-2xl lg:text-3xl text1 font-bold'>Click this link to join WhatsApp Group</h1>
+          <div className='flex space-x-4 mt-5 xl:mt-0'>
+            <div className="icon h-10 w-10  text-2xl md:text-3xl flex justify-center items-center bg-red-500 text-white rounded-md ml-3 md:ml-5">
+              <FaArrowRight />
+            </div>
+            <div className="icon h-10 w-10  text-2xl md:text-3xl  flex justify-center items-center bg-red-500 text-white rounded-md ml-3 md:ml-5">
+              <FaArrowRight />
+            </div>
+
+          </div>
+        </div>
+        <a href="https://chat.whatsapp.com/ElzWGqAuASp57bOhevsC8y" className="h-20 w-20 md:h-24 md:w-28 block mt-6 xl:mt-0">
+          <img src={Logo1} className="w-full h-full" alt="WhatsApp" />
+        </a>
+      </div>
+
+      {/* WhatsApp Number Section */}
+      <div className="w-full md:w-[90%] mx-auto mt-6 flex flex-wrap justify-center xl:justify-between lg:justify-between md:justify-between sm:justify-between  items-center flex-col xl:flex-row lg:flex-row md:flex-col  p-4 shadow-lg rounded-lg bg-white text-red-500">
+        <div className=" flex items-center flex-wrap flex-col xl:flex-row lg:flex-row">
+          <h1 className='text-xl md:text-2xl lg:text-3xl text1 font-bold'>WhatsApp Number: 03497104965</h1>
+          <div className='flex space-x-4 mt-5 xl:mt-0'>
+            <div className="icon h-10 w-10  text-2xl md:text-3xl flex justify-center items-center bg-red-500 text-white rounded-md ml-3 md:ml-5">
+              <FaArrowRight />
+            </div>
+            <div className="icon h-10 w-10  text-2xl md:text-3xl  flex justify-center items-center bg-red-500 text-white rounded-md ml-3 md:ml-5">
+              <FaArrowRight />
+            </div>
+          </div>
+        </div>
+        <div className="h-20 w-20 md:h-24 md:w-28 mt-6 xl:mt-0">
+          <img src={Logo2} className="w-full h-full" alt="WhatsApp" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App
